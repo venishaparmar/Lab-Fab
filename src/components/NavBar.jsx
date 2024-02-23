d32bde9dac337d469c16579b1a16a0759aa73eeb
Venisha Parmar <parmarvenisha725@gmail.com>
2024-02-23 13:07:56 +0530

    23-Feb

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,69 @@
-import "../styles/navbar.css";
-const NavBar = () => {
-  return (
-    <>
-      <header className="header top-0 bg-white shadow-md flex items-center justify-between px-60 py-02">
-        <h1 className="w-3/12">
-          <a href>
-            <img src="src\assets\images\logo.PNG" alt="logo" className="w-40" />
-          </a>
-        </h1>
-        <nav className="nav font-semibold text-lg">
-          <ul className="flex items-center">
-            <li className="p-4 border-b-2 border-blue-500 border-opacity-0 hover:border-opacity-100 hover:text-blue-500 duration-200 cursor-pointer active">
-              <a href>Home</a>
-            </li>
-            <li className="p-4 border-b-2 border-blue-500 border-opacity-0 hover:border-opacity-100 hover:text-blue-500 duration-200 cursor-pointer">
-              <a href="search.html">Search Resources</a>
-            </li>
-            <li className="p-4 border-b-2 border-blue-500 border-opacity-0 hover:border-opacity-100 hover:text-blue-500 duration-200 cursor-pointer">
-              <a href>Project</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="w-3/12 flex justify-end">
-          <img src="src\assets\images\ict_logo2.png" alt="ICT" id="ict-logo" />
-        </div>
-      </header>
-    </>
-  );
-};
+import { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Box, Button, MenuItem, Menu } from '@mui/material';
+import Logo from '../assets/images/logo.png'
+import IctLogo from  '../assets/images/ict_logo2.png';
 
+function NavBar() {
+    const my_pages = ['Solutions', 'Products', 'Pricing'];
+    const my_settings = ['Profile', 'Account', 'Logout'];
+
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenSettingsMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseSettingsMenu = () => {
+        setAnchorElUser(null);
+    };
+    return (
+        <AppBar position="static" sx={{ backgroundColor: 'white' }}>
+            <Toolbar>
+                <img src={Logo} style={{
+                    width: 150,
+                    height: 50,
+                    marginRight: 10,
+                    cursor: 'pointer',
+                }} />
+                <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+                    {my_pages.map((page) => (
+                        <Button
+                            key={page}
+                            sx={{ mx: 2, color: 'black' }}
+                        >
+                            {page}
+                        </Button>
+                    ))}
+                </Box>
+                <Box>
+                    <img
+                        src={IctLogo}
+                        style={{ width: 50, height: 50, cursor: 'pointer' }}
+                        onClick={handleOpenSettingsMenu}
+                    />
+                    <Menu
+                        sx={{ mt: '40px' }}
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseSettingsMenu}
+                    >
+                        {my_settings.map((setting) => (
+                            <MenuItem key={setting} onClick={handleCloseSettingsMenu}>
+                                <Typography textAlign="center">{setting}</Typography>
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
 export default NavBar;
