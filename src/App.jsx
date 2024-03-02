9fe379fe60c90b71798f6209035934c1f7de68fd
dpmehta <dpmehta2306@gmail.com>
2024-03-02 17:22:18 +0530

    2 march

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <NavBar />
-<<<<<<< HEAD
       {/* <HomeMain /> */}
       {/* <div class="container">
         <div class="row">
@@ -157,9 +156,9 @@ function App() {
           </div>
         </div>
       </div> */}
-=======
-      <HomeMain />
-     <div className="container">
+
+      {/* <HomeMain /> */}
+      {/* <div className="container">
   <div className="row">
     <div className="card col-lg-3 mt-3 mr-6">
       <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
@@ -279,9 +278,7 @@ function App() {
       </div>
     </div>
   </div>
-</div>
-
->>>>>>> 1517435fcd3320b1a6d658fdd40f4640e1359c02
+</div> */}
       {/* <LoginPage /> */}
       <LabEntry />
     </>
