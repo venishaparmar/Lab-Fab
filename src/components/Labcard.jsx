3b75185164e18abc289ef8077c5815f8a369efcd
Venisha Parmar <parmarvenisha725@gmail.com>
2024-03-03 12:50:10 +0530

    3-march

--- src/components/Labcard.jsx
+++ src/components/Labcard.jsx
@@ -5,28 +5,28 @@ export default function Labcard() {
         <div>
             <div className="card-list">
                 <a href="#" className="card-item">
-                    <img src="../assets/images/background.png" alt="Card Image" />
-                    <span className="developer">Developer</span>
-                    <h3>A developer codes software and websites.</h3>
-                    <div className="arrow">
-                        <i className="fas fa-arrow-right card-icon"></i>
-                    </div>
+                    <h2>MA115</h2>
+                    <h3>Web Developement Lab</h3>
                 </a>
                 <a href="#" className="card-item">
-                    <img src="" alt="Card Image" />
-                    <span className="designer">Designer</span>
-                    <h3>A designer is a design expert.</h3>
-                    <div className="arrow">
-                        <i className="fas fa-arrow-right card-icon"></i>
-                    </div>
+                    <h2>MA113</h2>
+                    <h3>Designer Lab </h3>
                 </a>
                 <a href="#" className="card-item">
-                    <img src="" alt="Card Image" />
-                    <span className="editor">Editor</span>
-                    <h3>An editor ensures content quality and accuracy.</h3>
-                    <div className="arrow">
-                        <i className="fas fa-arrow-right card-icon"></i>
-                    </div>
+                    <h2>MA112</h2>
+                    <h3>Data Science Lab </h3>
+                </a>
+                <a href="#" className="card-item">
+                    <h2>MA109</h2>
+                    <h3>Electronics Lab</h3>
+                </a>
+                <a href="#" className="card-item">
+                    <h2>MA108</h2>
+                    <h3>Communication Lab</h3>
+                </a>
+                <a href="#" className="card-item">
+                    <h2>MA107</h2>
+                    <h3>Robotics Lab</h3>
                 </a>
             </div>
         </div>
