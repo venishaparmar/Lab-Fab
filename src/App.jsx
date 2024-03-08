140625f6f3dcbfff0c27281c6a101909ea3fc0e3
Venisha Parmar <parmarvenisha725@gmail.com>
2024-03-08 20:52:44 +0530

    8-March

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,27 @@ import "../src/styles/App.css";
 import Labs from "./pages/Labs";
 import NavBar from "./components/NavBar";
 import HomeMain from "./components/HomeMain";
+import LoginPage from "./components/LoginPage";
 
 
 
 function App() {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       <>
-        <NavBar />
+        {/* <NavBar /> */}
 
         <Routes>
-          <Route path="/" element={<HomeMain />}/>
-          <Route path="/labs" element={<Labs />}/>
-            {/* <Route path="project" element={<Project />} /> */}
-          
+          {/* <Route path="/" element={<HomeMain />}/>
+          <Route path="/labs" element={<Labs />}/> */}
+          {/* <Route path="project" element={<Project />} /> */}
         </Routes>
-        </>
-        {/* <Labs/> */}
-      {/* <Labcard/> */}
-      {/* <NavBar />*/}
-      {/* <LabEntry /> */}
-      {/* <HomeMain /> */}
-      {/* <LoginPage /> */}
 
-      </BrowserRouter>
-      
+      </>
+
+      <LoginPage />
+    </BrowserRouter>
+
   );
 }
 
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useState } from 'react';
 import "../styles/login.css";
+import { Icon } from 'react-icons-kit';
+import { eyeOff } from 'react-icons-kit/feather/eyeOff';
+import { eye } from 'react-icons-kit/feather/eye'
 
 const LoginPage = () => {
+
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState('password');
+  const [icon, setIcon] = useState(eyeOff);
+  const handleToggle = () => {
+    if (type === 'password') {
+      setIcon(eye);
+      setType('text')
+    } else {
+      setIcon(eyeOff)
+      setType('password')
+    }
+  }
   return (
     <>
       <div className="background">
@@ -11,12 +27,25 @@ const LoginPage = () => {
         </div>
       </div>
       <form>
-        <h3>Login Here</h3>
+        <h1>Welcome to MU ICT Labs</h1>
+        <h5>Login to access your account</h5>
         <div>
           <label htmlFor="username">Username</label>
           <input type="text" placeholder="Email or Phone" id="username" />
-          <label htmlFor="password">Password</label>
-          <input type="password" placeholder="Password" id="password" />
+          <label htmlFor="username">Password</label>
+          <div className="mb-4 flex">
+            <input
+              type={type}
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
+            />
+            <span className="flex justify-around items-center" onClick={handleToggle}>
+              <Icon class="absolute mr-10" icon={icon} size={20} />
+            </span>
+          </div>
           <button>Log In</button>
         </div>
       </form>
