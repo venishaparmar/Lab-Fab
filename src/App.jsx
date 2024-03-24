7c7e1c4029910aa349c659ad0621cb14c3120f17
dpmehta <dpmehta2306@gmail.com>
2024-03-24 08:51:01 +0530

    24-march

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/styles/App.css";
 // import Labs from "./pages/Labs";
 import NavBar from "./components/NavBar";
-// import HomeMain from "./components/HomeMain";
-
+//import HomeMain from "./components/HomeMain";
+import "./firebaseConfig";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 // import LoginPage from "./components/LoginPage";
-import ComponentIssue from "./components/ComponentIssue";
+//import ComponentIssue from "./components/ComponentIssue";
+import { useState } from "react";
 
 function App() {
+  const db = getFirestore();
+
+  const [storedValues, setStoredValues] = useState([]);
+  const fetchDataFromFirestore = async () => {
+    const querySnapshot = await getDocs(collection(db, "project-details"));
+    const tmparray = [];
+    querySnapshot.forEach((doc) => {
+      tmparray.push(doc.data());
+    });
+    setStoredValues(tmparray);
+    console.log(tmparray);
+  };
   return (
     <BrowserRouter>
       <>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<HomeMain />} />
-          {/* <Route path="/labs" element={<Labs />} /> */}
-
-          {/* <Route path="/" element={<HomeMain />}/>
-          <Route path="/labs" element={<Labs />}/> */}
-
-          {/* <Route path="project" element={<Project />} /> */}
-        </Routes>
+        <Routes>{/* <Route path="/" element={<HomeMain />} /> */}</Routes>
       </>
-      <ComponentIssue />
+      {/* <ComponentIssue /> */}
+      <button onClick={fetchDataFromFirestore}> Fetch Data </button>
       {/* <LoginPage /> */}
     </BrowserRouter>
   );
