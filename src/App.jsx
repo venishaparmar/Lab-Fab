b1678d04f477be5ece84725317d2fb811086b87d
dpmehta <dpmehta2306@gmail.com>
2024-03-24 14:47:32 +0530

    24-march

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,20 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/styles/App.css";
-// import Labs from "./pages/Labs";
-import NavBar from "./components/NavBar";
-//import HomeMain from "./components/HomeMain";
-import "./firebaseConfig";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
-// import LoginPage from "./components/LoginPage";
-//import ComponentIssue from "./components/ComponentIssue";
-import { useState } from "react";
-=======
-<<<<<<< HEAD
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
-import { BrowserRouter, Routes } from "react-router-dom";
->>>>>>> 442723ffe7ec56c07ab5b029b752173339e2cb28
-import "../src/styles/App.css";
-import Labs from "./pages/Labs";
 import NavBar from "./components/NavBar";
 import HomeMain from "./components/HomeMain";
-
-<<<<<<< HEAD
-
-import LoginPage from "./components/LoginPage";
-=======
 // import LoginPage from "./components/LoginPage";
->>>>>>> 442723ffe7ec56c07ab5b029b752173339e2cb28
-import ComponentIssue from "./components/ComponentIssue";
->>>>>>> 88524739afd00b8ff0940a36332253f1aada23d7
+//import ComponentIssue from "./components/ComponentIssue";
 
 function App() {
-  const db = getFirestore();
-
-  const [storedValues, setStoredValues] = useState([]);
-  const fetchDataFromFirestore = async () => {
-    const querySnapshot = await getDocs(collection(db, "project-details"));
-    const tmparray = [];
-    querySnapshot.forEach((doc) => {
-      tmparray.push(doc.data());
-    });
-    setStoredValues(tmparray);
-    console.log(tmparray);
-  };
   return (
     <BrowserRouter>
       <>
-<<<<<<< HEAD
         <NavBar />
-        <Routes>{/* <Route path="/" element={<HomeMain />} /> */}</Routes>
-      </>
-      {/* <ComponentIssue /> */}
-      <button onClick={fetchDataFromFirestore}> Fetch Data </button>
-=======
-        {/* <NavBar /> */}
-        {/* <HomeMain /> */}
         <Routes>
-<<<<<<< HEAD
-          <Route path="/" element={<HomeMain />}/>
-          {/* <Route path="/labs" element={<Labs />}/> 
-          <Route path="project" element={<Project />} /> */}
-=======
           <Route path="/" element={<HomeMain />} />
-          {/* <Route path="/labs" element={<Labs />} /> */}
-
-          {/* <Route path="/" element={<HomeMain />}/>
-          <Route path="/labs" element={<Labs />}/> */}
-
-          {/* <Route path="project" element={<Project />} /> */}
->>>>>>> 442723ffe7ec56c07ab5b029b752173339e2cb28
         </Routes>
       </>
       {/* <ComponentIssue /> */}
->>>>>>> 88524739afd00b8ff0940a36332253f1aada23d7
-      {/* <LoginPage /> */}
     </BrowserRouter>
   );
 }
--- src/components/Homecard.jsx
+++ src/components/Homecard.jsx
@@ -1,130 +1,44 @@
 import "../styles/App.css";
+import "../firebaseConfig";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 export default function Homecard() {
-    return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="card col-lg-3 mt-3 mr-6">
-                        <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
-                        <div className="card__content">
-                            <p className="card__title">Project Name</p>
-                            <p className="card__description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation ullamco.
-                            </p>
-                            <button className="card__button">Live Demo</button>
-                            <button className="card__button secondary">Source Code</button>
-                        </div>
-                    </div>
-                    <div className="card col-lg-3 mt-3 mr-6">
-                        <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
-                        <div className="card__content">
-                            <p className="card__title">Project Name</p>
-                            <p className="card__description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation ullamco.
-                            </p>
-                            <button className="card__button">Live Demo</button>
-                            <button className="card__button secondary">Source Code</button>
-                        </div>
-                    </div>
-                    <div className="card col-lg-3 mt-3 mr-6">
-                        <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
-                        <div className="card__content">
-                            <p className="card__title">Project Name</p>
-                            <p className="card__description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation ullamco.
-                            </p>
-                            <button className="card__button">Live Demo</button>
-                            <button className="card__button secondary">Source Code</button>
-                        </div>
-                    </div>
-                    <div className="card col-lg-3 mt-3 mr-6">
-                        <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
-                        <div className="card__content">
-                            <p className="card__title">Project Name</p>
-                            <p className="card__description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation ullamco.
-                            </p>
-                            <button className="card__button">Live Demo</button>
-                            <button className="card__button secondary">Source Code</button>
-                        </div>
-                    </div>
-                    <div className="card col-lg-3 mt-3 mr-6">
-                        <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
-                        <div className="card__content">
-                            <p className="card__title">Project Name</p>
-                            <p className="card__description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation ullamco.
-                            </p>
-                            <button className="card__button">Live Demo</button>
-                            <button className="card__button secondary">Source Code</button>
-                        </div>
-                    </div>
-                    <div className="card col-lg-3 mt-3 mr-6">
-                        <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
-                        <div className="card__content">
-                            <p className="card__title">Project Name</p>
-                            <p className="card__description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation ullamco.
-                            </p>
-                            <button className="card__button">Live Demo</button>
-                            <button className="card__button secondary">Source Code</button>
-                        </div>
-                    </div>
-                    <div className="card col-lg-3 mt-3 mr-6">
-                        <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
-                        <div className="card__content">
-                            <p className="card__title">Project Name</p>
-                            <p className="card__description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation ullamco.
-                            </p>
-                            <button className="card__button">Live Demo</button>
-                            <button className="card__button secondary">Source Code</button>
-                        </div>
-                    </div>
-                    <div className="card col-lg-3 mt-3 mr-6">
-                        <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
-                        <div className="card__content">
-                            <p className="card__title">Project Name</p>
-                            <p className="card__description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation ullamco.
-                            </p>
-                            <button className="card__button">Live Demo</button>
-                            <button className="card__button secondary">Source Code</button>
-                        </div>
-                    </div>
-                    <div className="card col-lg-3 mt-3 mr-6">
-                        <img src="https://imgs.search.brave.com/1mBj-BB6QbpQ-iE8fjtTosHivzamuu1_ZKnhVro41dc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/Mzg5OTEzODMxNDIt/MzZjNGVkZWFmZmRl/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4Tm54OFpu/SmxaWHhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="hello" />
-                        <div className="card__content">
-                            <p className="card__title">Project Name</p>
-                            <p className="card__description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                enim ad minim veniam, quis nostrud exercitation ullamco....
-                            </p>
-                            <button className="card__button">Live Demo</button>
-                            <button className="card__button secondary">Source Code</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const db = getFirestore();
+  const [storedValues, setStoredValues] = useState([]);
+  const fetchDataFromFirestore = async () => {
+    const querySnapshot = await getDocs(collection(db, "resource-details"));
+    const tmparray = [];
+    querySnapshot.forEach((doc) => {
+      tmparray.push(doc.data());
+    });
+    setStoredValues(tmparray);
+  };
 
+  useEffect(() => {
+    fetchDataFromFirestore();
+  }, []);
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          {storedValues.map((element, index) => {
+            let newImagePath = "src/" + element.imagePath;
+            return (
+              <div key={index} className="card col-lg-3 mt-3 mr-6">
+                <img src={newImagePath} alt="hello" />
+                <div className="card__content">
+                  <p className="card__title">{element.name}</p>
+                  <p className="card__description">{element.description}</p>
+                  <button className="card__button">See Demo</button>
+                  <button className="card__button secondary">More</button>
+                </div>
+              </div>
+            );
+          })}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
