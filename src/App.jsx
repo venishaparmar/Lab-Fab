0b55ad7561d4be149598f39a3331155b1cdc5986
Venisha Parmar <parmarvenisha725@gmail.com>
2024-03-24 09:13:06 +0530

    24-march

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "../src/styles/App.css";
-// import Labs from "./pages/Labs";
 import NavBar from "./components/NavBar";
-//import HomeMain from "./components/HomeMain";
 import "./firebaseConfig";
 import { collection, getDocs, getFirestore } from "firebase/firestore";
-// import LoginPage from "./components/LoginPage";
-//import ComponentIssue from "./components/ComponentIssue";
 import { useState } from "react";
-=======
-<<<<<<< HEAD
->>>>>>> c1ae2bd1b7643fb19a26465d81d457047593b77f
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/styles/App.css";
 import Labs from "./pages/Labs";
-import NavBar from "./components/NavBar";
+
 import HomeMain from "./components/HomeMain";
 
-
-import LoginPage from "./components/LoginPage";
-import ComponentIssue from "./components/ComponentIssue";
->>>>>>> 88524739afd00b8ff0940a36332253f1aada23d7
 
 function App() {
   const db = getFirestore();
@@ -42,28 +25,15 @@ function App() {
   return (
     <BrowserRouter>
       <>
-<<<<<<< HEAD
         <NavBar />
         <Routes>{/* <Route path="/" element={<HomeMain />} /> */}</Routes>
       </>
-      {/* <ComponentIssue /> */}
       <button onClick={fetchDataFromFirestore}> Fetch Data </button>
-=======
-        {/* <NavBar /> */}
-        {/* <HomeMain /> */}
         <Routes>
-          {/* <Route path="/" element={<HomeMain />}/> */}
-          {/* <Route path="/labs" element={<Labs />}/> 
-          <Route path="project" element={<Project />} /> */}
+          <Route path="/" element={<HomeMain />}/>
+          <Route path="/labs" element={<Labs />}/> 
+          {/* <Route path="project" element={<Project />} /> */}
         </Routes>
-      </>
-      {/* <ComponentIssue /> */}
-<<<<<<< HEAD
-      <LoginPage />
-=======
->>>>>>> 88524739afd00b8ff0940a36332253f1aada23d7
-      {/* <LoginPage /> */}
->>>>>>> c1ae2bd1b7643fb19a26465d81d457047593b77f
     </BrowserRouter>
   );
 }
