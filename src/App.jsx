532c00478ad8a25c23fa89f3e63316d9b8972317
Venisha Parmar <parmarvenisha725@gmail.com>
2024-03-24 08:07:56 +0530

    24-march

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/styles/App.css";
-// import Labs from "./pages/Labs";
+import Labs from "./pages/Labs";
 import NavBar from "./components/NavBar";
-// import HomeMain from "./components/HomeMain";
+import HomeMain from "./components/HomeMain";
 
-// import HomeMain from "./components/HomeMain";
 
-// import LoginPage from "./components/LoginPage";
+import LoginPage from "./components/LoginPage";
 import ComponentIssue from "./components/ComponentIssue";
 
 function App() {
   return (
     <BrowserRouter>
       <>
-        <NavBar />
+        {/* <NavBar /> */}
+        {/* <HomeMain /> */}
         <Routes>
-          {/* <Route path="/" element={<HomeMain />}/>
-          <Route path="/labs" element={<Labs />}/> */}
-          {/* <Route path="project" element={<Project />} /> */}
+          <Route path="/" element={<HomeMain />}/>
+          {/* <Route path="/labs" element={<Labs />}/> 
+          <Route path="project" element={<Project />} /> */}
         </Routes>
       </>
-      <ComponentIssue />
+      {/* <ComponentIssue /> */}
       {/* <LoginPage /> */}
     </BrowserRouter>
   );
