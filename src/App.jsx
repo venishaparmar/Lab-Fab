442723ffe7ec56c07ab5b029b752173339e2cb28
dpmehta <dpmehta2306@gmail.com>
2024-03-24 08:01:56 +0530

    24-march

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-
 import { BrowserRouter, Routes } from "react-router-dom";
 import "../src/styles/App.css";
 // import Labs from "./pages/Labs";
 import NavBar from "./components/NavBar";
-// import HomeMain from "./components/HomeMain";
-
 // import HomeMain from "./components/HomeMain";
 
 // import LoginPage from "./components/LoginPage";
@@ -16,13 +13,12 @@ function App() {
       <>
         <NavBar />
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<HomeMain />} />
           {/* <Route path="/labs" element={<Labs />} /> */}
-=======
+
           {/* <Route path="/" element={<HomeMain />}/>
           <Route path="/labs" element={<Labs />}/> */}
->>>>>>> 69c92c81c8bb74c226d4d2bfe834127f09b31ed5
+
           {/* <Route path="project" element={<Project />} /> */}
         </Routes>
       </>
