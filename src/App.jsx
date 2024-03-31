4f6c335bcd7d2970601184bd2d3cf492bead9b0b
Venisha Parmar <parmarvenisha725@gmail.com>
2024-03-31 14:55:53 +0530

    31-March

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/styles/App.css";
 // import NavBar from "./components/NavBar";
@@ -7,21 +6,25 @@ import LoginPage from "./components/LoginPage";
 import ComponentIssue from "./components/ComponentIssue";
 import LabEntry from "./components/LabEntry";
 import SignUp from "./components/SignUp";
+import ComponentPage from "./components/ComponentPage";
 // import Labcard from "./components/Labcard";
 
 function App() {
   return (
-    <BrowserRouter>
-      <>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/home" element={<HomeMain />} />
-          <Route path="/component-issue" element={<ComponentIssue />} />
-          <Route path="/lab-entry" element={<LabEntry />} />
-        </Routes>
-      </>
-    </BrowserRouter>
+    <>
+    <ComponentPage/>
+    </>
+    // <BrowserRouter>
+    //   <>
+    //     <Routes>
+    //       <Route path="/login" element={<LoginPage />} />
+    //       <Route path="/signup" element={<SignUp />} />
+    //       <Route path="/home" element={<HomeMain />} />
+    //       <Route path="/component-issue" element={<ComponentIssue />} />
+    //       <Route path="/lab-entry" element={<LabEntry />} />
+    //     </Routes>
+    //   </>
+    // </BrowserRouter>
   );
 }
 
