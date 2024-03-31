436297bb33b8d798a73179573c0fd704c9613f39
dpmehta <dpmehta2306@gmail.com>
2024-03-31 09:34:34 +0530

    31-march

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../src/styles/App.css";
-import NavBar from "./components/NavBar";
+// import NavBar from "./components/NavBar";
 import HomeMain from "./components/HomeMain";
 import LoginPage from "./components/LoginPage";
 import ComponentIssue from "./components/ComponentIssue";
 import LabEntry from "./components/LabEntry";
-
-
-import Labcard from "./components/Labcard";
+// import Labcard from "./components/Labcard";
 
 function App() {
- 
-
   return (
     <BrowserRouter>
       <>
-        <NavBar />
-  
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomeMain />} />
