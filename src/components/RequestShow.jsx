7cea9717e548a4118c3d06d4f4801089ac6f7691
Venisha Parmar <parmarvenisha725@gmail.com>
2024-04-18 16:48:37 +0530

    created request show table.

--- src/components/RequestShow.jsx
+++ src/components/RequestShow.jsx
@@ -1,108 +1,283 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import NavBar from "./NavBar";
-        
+import React, { useState, useEffect } from 'react';
+import { classNames } from 'primereact/utils';
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
+import { Dropdown } from 'primereact/dropdown';
+import { MultiSelect } from 'primereact/multiselect';
+import { Tag } from 'primereact/tag';
+import { TriStateCheckbox } from 'primereact/tristatecheckbox';
+import { CustomerService } from './CustomerService';
+import "../styles/RequestShow.css"
+import { Row } from 'primereact/row';
 
-const RequestShow = () => {
-  const [componentIssues, setComponentIssues] = useState([]);
-  const [error, setError] = useState(null);
+export default function BasicFilterDemo() {
+    const [customers, setCustomers] = useState(null);
+    const [filters, setFilters] = useState({
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        'country.name': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        representative: { value: null, matchMode: FilterMatchMode.IN },
+        status: { value: null, matchMode: FilterMatchMode.EQUALS },
+        verified: { value: null, matchMode: FilterMatchMode.EQUALS }
+    });
+    const [loading, setLoading] = useState(true);
+    const [globalFilterValue, setGlobalFilterValue] = useState('');
+    const [representatives] = useState([
+        { name: 'Amy Elsner', image: 'amyelsner.png' },
+        { name: 'Anna Fali', image: 'annafali.png' },
+        { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+        { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+        { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+        { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+        { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+        { name: 'Onyama Limba', image: 'onyamalimba.png' },
+        { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+        { name: 'XuXue Feng', image: 'xuxuefeng.png' }
+    ]);
+    const [statuses] = useState(['unqualified', 'qualified', 'new', 'negotiation', 'renewal']);
 
-  useEffect(() => {
-    const fetchComponentIssues = async () => {
-      try {
-        // Replace 'YOUR_STUDENT_TOKEN' with the actual student token value
-        const studentToken = localStorage.getItem("jwt-token");
-        console.log(studentToken);
-        const response = await axios.get(
-          `http://localhost:3000/api/component/component-issues/e1bscdhvbcgft2`
-        );
-        setComponentIssues(response.data.componentIssues);
-        console.log(response.data.componentIssues);
-      } catch (error) {
-        setError(error.response.data.message);
-      }
+    const getSeverity = (status) => {
+        switch (status) {
+            case 'unqualified':
+                return 'danger';
+
+            case 'qualified':
+                return 'success';
+
+            case 'new':
+                return 'info';
+
+            case 'negotiation':
+                return 'warning';
+
+            case 'renewal':
+                return null;
+        }
     };
 
-    fetchComponentIssues();
-  }, []);
-  return (
-    <>
-      <NavBar />
-      <div className="flex flex-col items-center justify-center mt-3">
-        <main className="min-h-screen">
-          <div className="grid w-[40rem] grid-cols-3 gap-2 rounded-xl bg-gray-200 p-2">
-            <div className="mb-2 ml-2">
-              <input
-                type="radio"
-                name="option"
-                id={1}
-                defaultValue={1}
-                className="peer hidden"
-                defaultChecked
-              />
-              <label
-                htmlFor={1}
-                className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
-              >
-                All Requests
-              </label>
+    useEffect(() => {
+        CustomerService.getCustomersMedium().then((data) => {
+            setCustomers(getCustomers(data));
+            setLoading(false);
+        });
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    const getCustomers = (data) => {
+        return [...(data || [])].map((d) => {
+            d.date = new Date(d.date);
+
+            return d;
+        });
+    };
+
+    const onGlobalFilterChange = (e) => {
+        const value = e.target.value;
+        let _filters = { ...filters };
+
+        _filters['global'].value = value;
+
+        setFilters(_filters);
+        setGlobalFilterValue(value);
+    };
+
+    const renderHeader = () => {
+        return (
+            <div className="flex justify-content-end">
+                <span className="p-input-icon-left">
+                    <i className="pi pi-search" />
+                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
+                </span>
             </div>
-            <div>
-              <input
-                type="radio"
-                name="option"
-                id={2}
-                defaultValue={2}
-                className="peer hidden"
-              />
-              <label
-                htmlFor={2}
-                className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
-              >
-                Pending Requests
-              </label>
+        );
+    };
+
+    const countryBodyTemplate = (rowData) => {
+        return (
+            <div className="flex align-items-center gap-2">
+                <img alt="flag" src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png" className={`flag flag-${rowData.country.code}`} style={{ width: '24px' }} />
+                <span>{rowData.country.name}</span>
             </div>
-            <div className="mr-2">
-              <input
-                type="radio"
-                name="option"
-                id={3}
-                defaultValue={3}
-                className="peer hidden"
-              />
-              <label
-                htmlFor={3}
-                className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
-              >
-                Accepted Request
-              </label>
+        );
+    };
+
+    const representativeBodyTemplate = (rowData) => {
+        const representative = rowData.representative;
+
+        return (
+            <div className="flex align-items-center gap-2">
+                <img alt={representative.name} src={`https://primefaces.org/cdn/primereact/images/avatar/${representative.image}`} width="32" />
+                <span>{representative.name}</span>
             </div>
-          </div>
-        </main>
+        );
+    };
+
+    const representativesItemTemplate = (option) => {
+        return (
+            <div className="flex align-items-center gap-2">
+                <img alt={option.name} src={`https://primefaces.org/cdn/primereact/images/avatar/${option.image}`} width="32" />
+                <span>{option.name}</span>
+            </div>
+        );
+    };
+
+    const statusBodyTemplate = (rowData) => {
+        return <Tag value={rowData.status} severity={getSeverity(rowData.status)} />;
+    };
+
+    const statusItemTemplate = (option) => {
+        return <Tag value={option} severity={getSeverity(option)} />;
+    };
+
+    const verifiedBodyTemplate = (rowData) => {
+        return <i className={classNames('pi', { 'true-icon pi-check-circle': rowData.verified, 'false-icon pi-times-circle': !rowData.verified })}></i>;
+    };
+
+    const representativeRowFilterTemplate = (options) => {
+        return (
+            <MultiSelect
+                value={options.value}
+                options={representatives}
+                itemTemplate={representativesItemTemplate}
+                onChange={(e) => options.filterApplyCallback(e.value)}
+                optionLabel="name"
+                placeholder="Any"
+                className="p-column-filter"
+                maxSelectedLabels={1}
+                style={{ minWidth: '14rem' }}
+            />
+        );
+    };
+
+    const statusRowFilterTemplate = (options) => {
+        return (
+            <Dropdown value={options.value} options={statuses} onChange={(e) => options.filterApplyCallback(e.value)} itemTemplate={statusItemTemplate} placeholder="Select One" className="p-column-filter" showClear style={{ minWidth: '12rem' }} />
+        );
+    };
+
+    const verifiedRowFilterTemplate = (options) => {
+        return <TriStateCheckbox value={options.value} onChange={(e) => options.filterApplyCallback(e.value)} />;
+    };
+
+    const header = renderHeader();
+
+    return (
+        <div className="table-card">
+            <DataTable value={customers} paginator rows={10} dataKey="id" filters={filters} filterDisplay="row" loading={loading}
+                    globalFilterFields={['name', 'country.name', 'representative.name', 'status']} header={header} emptyMessage="No customers found.">
+                <Column field="name" header="Name" filter filterPlaceholder="Search by name" style={{ minWidth: '12rem' }} />
+                <Column header="Country" filterField="country.name" style={{ minWidth: '12rem' }} body={countryBodyTemplate} filter filterPlaceholder="Search by country" />
+                <Column header="Agent" filterField="representative" showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '14rem' }}
+                    body={representativeBodyTemplate} filter filterElement={representativeRowFilterTemplate} />
+                <Row field="status" header="Status" showFilterMenu={false} filterMenuStyle={{ width: '14rem' }} style={{ minWidth: '12rem' }} body={statusBodyTemplate} filter filterElement={statusRowFilterTemplate} />
+                <Row field="verified" header="Verified" dataType="boolean" style={{ minWidth: '6rem' }} body={verifiedBodyTemplate} filter filterElement={verifiedRowFilterTemplate} />
+            </DataTable>
+        </div>
+    );
+}
+        
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import NavBar from "./NavBar";
+
+// const RequestShow = () => {
+//   const [componentIssues, setComponentIssues] = useState([]);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchComponentIssues = async () => {
+//       try {
+//         // Replace 'YOUR_STUDENT_TOKEN' with the actual student token value
+//         const studentToken = localStorage.getItem("jwt-token");
+//         console.log(studentToken);
+//         const response = await axios.get(
+//           `http://localhost:3000/api/component/component-issues/e1bscdhvbcgft2`
+//         );
+//         setComponentIssues(response.data.componentIssues);
+//         console.log(response.data.componentIssues);
+//       } catch (error) {
+//         setError(error.response.data.message);
+//       }
+//     };
+
+//     fetchComponentIssues();
+//   }, []);
+//   return (
+//     <>
+//       <NavBar />
+//       <div className="flex flex-col items-center justify-center mt-3">
+//         <main className="min-h-screen">
+//           <div className="grid w-[40rem] grid-cols-3 gap-2 rounded-xl bg-gray-200 p-2">
+//             <div className="mb-2 ml-2">
+//               <input
+//                 type="radio"
+//                 name="option"
+//                 id={1}
+//                 defaultValue={1}
+//                 className="peer hidden"
+//                 defaultChecked
+//               />
+//               <label
+//                 htmlFor={1}
+//                 className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
+//               >
+//                 All Requests
+//               </label>
+//             </div>
+//             <div>
+//               <input
+//                 type="radio"
+//                 name="option"
+//                 id={2}
+//                 defaultValue={2}
+//                 className="peer hidden"
+//               />
+//               <label
+//                 htmlFor={2}
+//                 className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
+//               >
+//                 Pending Requests
+//               </label>
+//             </div>
+//             <div className="mr-2">
+//               <input
+//                 type="radio"
+//                 name="option"
+//                 id={3}
+//                 defaultValue={3}
+//                 className="peer hidden"
+//               />
+//               <label
+//                 htmlFor={3}
+//                 className="block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white"
+//               >
+//                 Accepted Request
+//               </label>
+//             </div>
+//           </div>
+//         </main>
 
         
-      </div>
+//       </div>
+//       <div>
+//         {error && <p>Error: {error}</p>}
+//         <h2>Component Issue Requests</h2>
+//         <ul>
+//           {componentIssues.map((issue) => (
+//             <li key={issue._id}>
+//               <p>Component Name: {issue.componentName}</p>
+//               <p>Quantity: {issue.quantity}</p>
+//               <p>Issue Date: {issue.issueDate}</p>
+//               <p>Return Date: {issue.returnDate}</p>
+//               <p>Purpose: {issue.purpose}</p>
+//               <p>Status: {issue.status}</p>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </>
+//   );
+// };
 
-
-
-      <div>
-        {error && <p>Error: {error}</p>}
-        <h2>Component Issue Requests</h2>
-        <ul>
-          {componentIssues.map((issue) => (
-            <li key={issue._id}>
-              <p>Component Name: {issue.componentName}</p>
-              <p>Quantity: {issue.quantity}</p>
-              <p>Issue Date: {issue.issueDate}</p>
-              <p>Return Date: {issue.returnDate}</p>
-              <p>Purpose: {issue.purpose}</p>
-              <p>Status: {issue.status}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
-};
-
-export default RequestShow;
+// export default RequestShow;
