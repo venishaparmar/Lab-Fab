20da4d17e59774eb6cf234216911ad6c4f617497
Venisha Parmar <parmarvenisha725@gmail.com>
2024-04-23 13:15:52 +0530

    updated navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,10 +15,10 @@ export default function NavBar() {
   };
   return (
     <>
-      <header className="header top-0 bg-white shadow-md flex flex-col sm:flex-row items-center justify-between px-4 sm:px-6 lg:px-8 py-2 sticky top-0 z-50">
+      <header className="header top-0 bg-white shadow-md flex flex-col sm:flex-row items-center justify-between px-4 sm:px-6 lg:px-8 py-1 sticky top-0 z-50">
         <h1 className="w-full sm:w-4/12 sm:mb-0">
           <a href="/">
-            <img src={MuLogo} alt="logo" className="w-60 mx-auto sm:mx-0" />
+            <img src={MuLogo} alt="logo" className="w-50 mx-5 sm:mx-0" />
           </a>
         </h1>
         <nav className="nav font-semibold text-lg flex-grow">
@@ -34,6 +34,9 @@ export default function NavBar() {
             </li>
             <li className="p-4 b-2">
               <Link to="/labs">Labs</Link>
+            </li>
+            <li className="p-4 b-2">
+              <Link to="/request-show">My Requests</Link>
             </li>
           </ul>
         </nav>
