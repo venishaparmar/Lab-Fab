25f488b830f005728a5e8cb5288a5ce20e19f04c
Venisha Parmar <parmarvenisha725@gmail.com>
2024-04-24 23:08:11 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "../src/styles/App.css";
 import HomeMain from "./components/HomeMain";
 import LoginPage from "./components/LoginPage";
 import ComponentIssue from "./components/ComponentIssue";
-import SignUp from "./components/SignUp";
+import StudentSignUp from "./components/StudentSignUp";
 import ComponentPage from "./components/ComponentPage";
 import RequestShow from "./components/RequestShow";
 import QrReader from "./components/QrReader";
@@ -11,6 +11,9 @@ import Labs from "./components/Labs";
 import LabEntry from "./components/LabEntry";
 import { Toaster } from "react-hot-toast";
 import ComponentReview from "./components/ComponentReview";
+import FacultySignUp from "./components/FacultySignUp";
+import FacultyLogin from "./components/FacultyLogin";
+import FacultyHome from "./components/FacultyHome";
 
 function App() {
   return (
@@ -18,19 +21,30 @@ function App() {
       <>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<SignUp />} />
+          <Route path="/student-signup" element={<StudentSignUp />} />
           <Route path="/home" element={<HomeMain />} />
           <Route path="/component-issue" element={<ComponentIssue />} />
           <Route path="/labs" element={<Labs />} />
           <Route path="/lab-entry" element={<LabEntry />} />
           <Route path="/request-show" element={<RequestShow />} />
-          <Route exact path="/component-page/:id" element={<ComponentPage />} />
-          <Route exact path="/qr-reader" element={<QrReader />} />
-          <Route exact path="/request" element={<RequestShow />} />
+          <Route path="/component-page/:id" element={<ComponentPage />} />
+          <Route path="/qr-reader" element={<QrReader />} />
+          <Route path="/request" element={<RequestShow />} />
           <Route
-            exact
-            path="component-request-review"
+            path="/component-request-review"
             element={<ComponentReview />}
+          />
+          <Route
+            path="/faculty-signup"
+            element={<FacultySignUp />}
+          />
+          <Route
+            path="/faculty-login"
+            element={<FacultyLogin />}
+          />
+          <Route
+            path="/faculty-home"
+            element={<FacultyHome />}
           />
         </Routes>
         <Toaster position="top-center" reverseOrder={false} />
