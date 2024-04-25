90a8b173856bf018bad6750276d9ba80faf26fb9
Venisha Parmar <parmarvenisha725@gmail.com>
2024-04-25 11:31:18 +0530

    updated navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,49 +16,54 @@ export default function NavBar() {
   return (
     <>
       <header className="header top-0 bg-white shadow-md flex flex-col sm:flex-row items-center justify-between px-4 sm:px-6 lg:px-8 py-1 sticky top-0 z-50">
-        <h1 className="w-full sm:w-4/12 sm:mb-0">
+        <div className="logo w-full sm:w-4/12 sm:mb-0">
           <a href="/">
             <img src={MuLogo} alt="logo" className="w-50 mx-5 sm:mx-0" />
+
           </a>
-        </h1>
+          <a href="/">
+            <img
+              src={IctLogo}
+              alt="ICT"
+              id="ict-logo"
+              className="w-30  lg:mr-30"
+            />
+          </a>
+        </div>
+        
         <nav className="nav font-semibold text-lg flex-grow">
-          <ul className="flex items-center justify-center sm:justify-end">
-            <li className="p-4 b-2">
+          <ul className="flex">
+
+            <li className="p-4 b-2 flex-shrink-0">
               <Link to="/home">Home</Link>
             </li>
-            <li className="p-4 b-2">
+            <li className="p-4 b-2 flex-shrink-0">
               <Link to="/lab-entry">Lab Entry</Link>
             </li>
-            <li className="p-4 b-2">
+            <li className="p-4 b-2 flex-shrink-0">
               <Link to="/component-issue">Component Issue</Link>
             </li>
-            <li className="p-4 b-2">
+            <li className="p-4 b-2 flex-shrink-0">
               <Link to="/labs">Labs</Link>
             </li>
-            <li className="p-4 b-2">
+            <li className="p-4 b-2 flex-shrink-0">
               <Link to="/request-show">My Requests</Link>
-            </li>
-            <li className="p-4 b-2">
-              <Link to="/component-request-review">Request Review</Link>
             </li>
           </ul>
         </nav>
+
         <div className="w-full sm:w-3/12 flex justify-center  sm:justify-end">
+
           {isLoggedIn && (
             <button
               onClick={handleLogout}
               type="button"
               className="btn btn-info mb-3 mr-3"
+              style={{ backgroundColor: '#14a2b9', width: '100px' }}
             >
               Logout
             </button>
           )}
-          <img
-            src={IctLogo}
-            alt="ICT"
-            id="ict-logo"
-            className="w-30  lg:mr-30"
-          />
         </div>
       </header>
     </>
